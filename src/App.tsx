6c5e4df55222d0cf894b6d5c94ba97612ff653eb
Kevin Lee <kevinlee3627@gmail.com>
2026-01-25 21:04:27 -0600

    feat: start on a UI

--- src/App.tsx
+++ src/App.tsx
@@ -9,37 +9,47 @@ function App() {
   const project = useAppSelector((state) => state.project);
   return (
     <GlobalAudioContextProvider>
-      <div style={{ marginBottom: '3rem' }}>
-        <div>
-          <p>Info:</p>
-          <p>Total Duration: {project.totalDuration}s</p>
-          <p>Total Measures: {project.totalMeasures}</p>
-          <p>BPM: {project.beatsPerMinute}</p>
-          <p>Beat Value: {project.beatValue}</p>
-          <p>Beats Per Measure: {project.beatsPerMeasure}</p>
-          <p>Seconds per measure: {project.secondsPerMeasure}</p>
-          <p>px per secondd: {project.pxPerSecondScale} px</p>
-          <p>px per measure: {project.pxPerMeasureScale} px</p>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div style={{ backgroundColor: 'red', height: '1.5rem' }}>toolbar</div>
+        <div style={{ marginBottom: '1rem' }}>
+          <div>
+            <p>Info:</p>
+            <p>Total Duration: {project.totalDuration}s</p>
+            <p>Total Measures: {project.totalMeasures}</p>
+            <p>BPM: {project.beatsPerMinute}</p>
+            <p>Beat Value: {project.beatValue}</p>
+            <p>Beats Per Measure: {project.beatsPerMeasure}</p>
+            <p>Seconds per measure: {project.secondsPerMeasure}</p>
+            <p>px per secondd: {project.pxPerSecondScale} px</p>
+            <p>px per measure: {project.pxPerMeasureScale} px</p>
+          </div>
+          <button
+            onClick={() => {
+              Object.values(tracks).forEach((track) =>
+                dispatch(
+                  trackSlice.actions.startTrack({ trackId: track.trackId }),
+                ),
+              );
+            }}
+          >
+            play all these tracks :D
+          </button>
+          <button onClick={() => dispatch(trackSlice.actions.addTrack())}>
+            add track
+          </button>
         </div>
-        <button
-          onClick={() => {
-            Object.values(tracks).forEach((track) =>
-              dispatch(
-                trackSlice.actions.startTrack({ trackId: track.trackId }),
-              ),
-            );
-          }}
-        >
-          play all these tracks :D
-        </button>
-        <button onClick={() => dispatch(trackSlice.actions.addTrack())}>
-          add track
-        </button>
-      </div>
-      <div style={{ height: '40%' }}>
-        {Object.keys(tracks).map((trackId) => {
-          return <Track trackId={trackId} key={trackId} />;
-        })}
+
+        <div style={{ flexGrow: 1 }}>
+          {Object.keys(tracks).map((trackId) => {
+            return <Track trackId={trackId} key={trackId} />;
+          })}
+        </div>
       </div>
     </GlobalAudioContextProvider>
   );
