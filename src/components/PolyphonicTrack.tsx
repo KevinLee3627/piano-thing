aa6e2cf2c18a6ae4423d9a3ac116ad784d02bca7
Kevin Lee <kevinlee3627@gmail.com>
2026-02-08 18:25:34 -0600

    feat: nothing

--- src/components/PolyphonicTrack.tsx
+++ src/components/PolyphonicTrack.tsx
@@ -1,8 +1,9 @@
 import { useAppSelector } from '@/app/hooks';
 import { cn } from '@/lib/utils';
-import { Keyboard } from './Keyboard';
 import { Block } from './Block';
 import type { useResizeObserver } from '@/hooks/useResizeObserver';
+import { noteMapping } from '@/util/noteUtils';
+import { useMemo } from 'react';
 
 interface PolyphonicTrackProps {
   trackId: string;
@@ -11,6 +12,29 @@ interface PolyphonicTrackProps {
 
 export const PolyphonicTrack = (props: PolyphonicTrackProps) => {
   const track = useAppSelector((state) => state.tracks[props.trackId]);
+
+  const notes = useMemo(() => {
+    // NOTE: Reverse b/c we want lower notes on the bottom
+    const octaves = [3, 4, 5].reverse();
+    return octaves.map((octave) => {
+      return Object.keys(noteMapping)
+        .reverse()
+        .map((noteName) => (
+          <div
+            key={`note-${noteName}${octave}`}
+            className={cn(
+              'h-6',
+              noteName.includes('#') ? 'bg-black' : 'bg-white',
+              noteName.includes('#') ? 'text-white' : 'text-black',
+            )}
+          >
+            {noteName}
+            {octave}
+          </div>
+        ));
+    });
+  }, []);
+
   return (
     <div
       className={cn(
@@ -19,6 +43,7 @@ export const PolyphonicTrack = (props: PolyphonicTrackProps) => {
         'overflow-auto',
       )}
     >
+      <div className='absolute'>{notes}</div>
       <div className='h-full relative'>
         {Object.entries(track.blocks).map(([blockId, block]) => (
           <Block
@@ -29,7 +54,7 @@ export const PolyphonicTrack = (props: PolyphonicTrackProps) => {
           />
         ))}
       </div>
-      <Keyboard trackId={props.trackId} />
+      {/* <Keyboard trackId={props.trackId} /> */}
     </div>
   );
 };
