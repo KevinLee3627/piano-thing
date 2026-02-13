028113315c5f0d393a82abdd21ca64c39f1dc128
Kevin Lee <kevinlee3627@gmail.com>
2026-02-13 17:14:37 -0600

    feat: quantize all notes when toggling on

--- src/components/TrackControl.tsx
+++ src/components/TrackControl.tsx
@@ -10,6 +10,7 @@ interface TrackControlProps {
 export const TrackControl = ({ trackId }: TrackControlProps) => {
   const dispatch = useAppDispatch();
   const track = useAppSelector((state) => state.tracks[trackId]);
+  const project = useAppSelector((state) => state.project);
 
   return (
     <div>
@@ -18,14 +19,38 @@ export const TrackControl = ({ trackId }: TrackControlProps) => {
       <Switch
         id={`track-${track.trackId}-quantize-toggle`}
         checked={track.isQuantized}
-        onCheckedChange={(checked) =>
+        onCheckedChange={(checked) => {
           dispatch(
             trackSlice.actions.setTrackQuantized({
               trackId: track.trackId,
               isQuantized: checked,
             }),
-          )
-        }
+          );
+          // TODO: Can we not make this 20 levels indented
+          // Quantizes all blocks when switching on
+          if (checked) {
+            const snapPointGap =
+              project.secondsPerMeasure /
+              project.beatsPerMeasure /
+              track.quantizationResolution;
+
+            Object.values(track.blocks).forEach((block) => {
+              const snappedStartTime =
+                Math.round(block.startTime / snapPointGap) * snapPointGap;
+              dispatch(
+                trackSlice.actions.editBlock({
+                  trackId: track.trackId,
+                  blockId: block.blockId,
+                  startTime: snappedStartTime,
+                  dims: {
+                    ...block.dims,
+                    left: snappedStartTime * project.pxPerSecondScale,
+                  },
+                }),
+              );
+            });
+          }
+        }}
       />
       <p>Q.Res: {track.quantizationResolution}</p>
     </div>
