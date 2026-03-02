917cf2f4d0c72f0bfd436e407126666d2185e141
Kevin Lee <kevinlee3627@gmail.com>
2026-03-01 21:21:22 -0600

    feat: start create project modal

--- src/components/TitleBar/CreateProjectDialog.tsx
+++ src/components/TitleBar/CreateProjectDialog.tsx
@@ -3,8 +3,18 @@ import {
   projectRegistrySlice,
   selectActiveProject,
 } from '@/app/projectRegistrySlice';
-import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from '../ui/dialog';
 import { MenubarItem } from '../ui/menubar';
+import { useState } from 'react';
+import { Button } from '../ui/button';
+import { DialogTitle } from '@radix-ui/react-dialog';
 
 export const CreateProjectDialog = () => {
   const dispatch = useAppDispatch();
@@ -12,29 +22,51 @@ export const CreateProjectDialog = () => {
   const activeProject = useAppSelector(selectActiveProject);
   const project = useAppSelector((state) => state.project);
   const tracks = useAppSelector((state) => state.tracks);
+
+  const [isOpen, setisOpen] = useState(false);
+
   return (
-    <Dialog>
-      <DialogTrigger>
+    <Dialog open={isOpen} onOpenChange={setisOpen}>
+      <DialogTrigger asChild>
         <MenubarItem
-          onClick={() => {
-            // dispatch(
-            //   projectRegistrySlice.actions.saveSnapshot({
-            //     id: activeProject.id,
-            //     projectState: project,
-            //     tracksState: tracks,
-            //   }),
-            // );
-            // dispatch(
-            //   projectRegistrySlice.actions.createProject({
-            //     name: 'Project',
-            //   }),
-            // );
+          onClick={(e) => {
+            e.preventDefault();
+            setisOpen(true);
           }}
         >
           New Project
         </MenubarItem>
       </DialogTrigger>
-      <DialogContent>Test</DialogContent>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Project</DialogTitle>
+        </DialogHeader>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={'outline'}>Cancel</Button>
+          </DialogClose>
+          <Button
+            onClick={() => {
+              dispatch(
+                projectRegistrySlice.actions.saveSnapshot({
+                  id: activeProject.id,
+                  projectState: project,
+                  tracksState: tracks,
+                }),
+              );
+              dispatch(
+                projectRegistrySlice.actions.createProject({
+                  name: 'Project',
+                }),
+              );
+              setisOpen(false);
+            }}
+          >
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 };
